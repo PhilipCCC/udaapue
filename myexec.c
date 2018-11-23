e49ab4b8bc063ff99a7fa9551dce0bde3453f7c0
Gilbert <x849247823@vip.qq.com>
2018-11-23 16:00:50 +0800

    change myexec.c to a correct.

--- myexec.c
+++ myexec.c
@@ -10,7 +10,7 @@ int main(void)
   if((pid = fork()) < 0){
     printf("fork error.\n");
   }else if (pid == 0){
-    if(execle("echoall", "echoall", "myarg1", "MY ARG2", (char*)0, env_init) < 0){
+    if(execle("/home/gilbert/bin/echoall", "echoall", "myarg1", "MY ARG2", (char*)0, env_init) < 0){
       printf("execle ereor");
       perror("a.out");
     }
@@ -20,7 +20,6 @@ int main(void)
     perror("waitpid");
     printf("wait pid ereor");
   }
-  /*
   if((pid = fork()) < 0)
     printf("fork2 error");
   else if (pid == 0){
@@ -28,6 +27,5 @@ int main(void)
       printf("execlp 2 error");
     }
   }
-  */
   exit(0);
 }
